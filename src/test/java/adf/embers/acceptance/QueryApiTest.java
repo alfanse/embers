9db93fb390d7d50201316095a94a1eed22f48547
alfanse <alex.fanshawe@gmail.com>
2014-12-30 22:32:03 +0000

    AF Getting gradle to set the yatspec output directory

--- src/test/java/adf/embers/acceptance/QueryApiTest.java
+++ src/test/java/adf/embers/acceptance/QueryApiTest.java
@@ -1,5 +1,18 @@
 package adf.embers.acceptance;
 
-public class QueryApiTest {
-	
+import com.googlecode.yatspec.junit.SpecRunner;
+import com.googlecode.yatspec.state.givenwhenthen.TestState;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(SpecRunner.class)
+public class QueryApiTest extends TestState {
+
+    @Test
+    public void querySingleTable() {
+        //given an app hosts embers
+        //when the user views show all reports
+        //then all reports are shown with name and descriptions
+    }
+
 }
