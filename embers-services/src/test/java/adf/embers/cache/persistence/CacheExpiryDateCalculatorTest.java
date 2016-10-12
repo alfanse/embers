3b754138d28d0e257e7cf748013aea1388da8de9
alfanse <alex.fanshawe@gmail.com>
2016-10-13 00:24:07 +0100

    binding dates to timezones for CacheExpiryCalculator

--- embers-services/src/test/java/adf/embers/cache/persistence/CacheExpiryDateCalculatorTest.java
+++ embers-services/src/test/java/adf/embers/cache/persistence/CacheExpiryDateCalculatorTest.java
@@ -3,11 +3,13 @@ package adf.embers.cache.persistence;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.time.Month;
 import java.time.ZoneId;
 import java.util.Date;
+import java.util.TimeZone;
 
 import static org.fest.assertions.api.Assertions.assertThat;
 
@@ -20,35 +22,48 @@ public class CacheExpiryDateCalculatorTest {
 
     @Before
     public void safeDateThatWontCrossDST() throws Exception {
-        startingDate = createDate(2016, Month.APRIL, 30);
+        startingDate = createLocalDate(2016, Month.APRIL, 30);
     }
 
     @Test
-    //noticed odd behaviour when test randomly failed for a certain date,
-    // turns out the api adds 1 hour when crosses daylight saving time
     public void addingMillisWhenBstStarts() throws Exception {
         long millisToAdd = ONE_HOUR_MS*48;
         //day before BST starts
-        Date dateBeforeDst = createDate(2016, Month.MARCH, 26);
+        Date dateBeforeDst = createLocalDate("2016-03-26 01:02", london);
 
         Date calculated = new CacheExpiryDateCalculator(dateBeforeDst, millisToAdd, london).invoke();
 
         //48 hours later, +1 hour for DST.
-        assertThat(calculated).withDateFormat(new SimpleDateFormat("yyyy-MM-dd HH:mm")).isEqualTo("2016-3-28 02:02");
+        assertThat(calculated).withDateFormat(setupSDF(london)).isEqualTo("2016-3-28 02:02");
+    }
+
+    @Test
+    public void addingMillisWhenBstFallsBack() throws Exception {
+        long millisToAdd = ONE_HOUR_MS*48;
+        //the day before British Summer Time fallback
+        Date start = createLocalDate(2016, Month.OCTOBER, 29);
+
+        Date calculated = new CacheExpiryDateCalculator(start, millisToAdd, london).invoke();
+
+        //48 hours later, -1 hour for DST fallback.
+        assertThat(calculated).withDateFormat(setupSDF(london)).isEqualTo("2016-10-31 00:02");
     }
 
     @Test
     //noticed odd behaviour when test randomly failed for a certain date,
     // turns out the api adds 1 hour when crosses daylight saving time
-    public void addingMillisWhenBstFallsBack() throws Exception {
-        long millisToAdd = ONE_HOUR_MS*48;
-        //the day before British Summer Time fallback
-        Date start = createDate(2016, Month.OCTOBER, 29);
+    //Sunday, 13 March 2016, 02:00:00 clocks were turned forward 1 hour to
+//    Sunday, 13 March 2016, 03:00:00 local daylight time instead
+    public void addingMillisWhenNewYorkBstStarts() throws Exception {
+        long millisToAdd = ONE_HOUR_MS*5;
+        //day before BST starts
+        ZoneId newYork = ZoneId.of("America/New_York");
+        Date from = createLocalDate("2016-03-13 01:02", newYork);
 
-        Date calculated = new CacheExpiryDateCalculator(start, millisToAdd, london).invoke();
+        Date calculated = new CacheExpiryDateCalculator(from, millisToAdd, newYork).invoke();
 
-        //48 hours later, -1 hour for DST fallback.
-        assertThat(calculated).withDateFormat(new SimpleDateFormat("yyyy-MM-dd HH:mm")).isEqualTo("2016-10-31 00:02");
+        //48 hours later, +1 hour for DST.
+        assertThat(calculated).withDateFormat(setupSDF(newYork)).isEqualTo("2016-3-13 07:02");
     }
 
     @Test
@@ -70,7 +85,18 @@ public class CacheExpiryDateCalculatorTest {
         new CacheExpiryDateCalculator(startingDate, millisToAdd).invoke();
     }
 
-    private Date createDate(final int year, final Month month, final int dayOfMonth) {
+    private SimpleDateFormat setupSDF(ZoneId zoneId) {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+        sdf.setTimeZone(TimeZone.getTimeZone(zoneId));
+        return sdf;
+    }
+
+    private Date createLocalDate(final String date, final ZoneId zoneId) throws ParseException {
+        SimpleDateFormat simpleDateFormat = setupSDF(zoneId);
+        return simpleDateFormat.parse(date);
+    }
+
+    private Date createLocalDate(final int year, final Month month, final int dayOfMonth) {
         LocalDateTime ldt = LocalDateTime.of(year, month, dayOfMonth, 1, 2);
         return Date.from(ldt.atZone(london).toInstant());
     }
