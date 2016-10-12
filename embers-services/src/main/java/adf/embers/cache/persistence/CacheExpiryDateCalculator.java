85e8cd8d961d3e6bf92ef7a01e8b19cb8d9016bc
alfanse <alex.fanshawe@gmail.com>
2016-10-12 22:59:46 +0100

    switching calculate cache expiration date to ZonedDateTime and unit testing across DST boundaries

--- embers-services/src/main/java/adf/embers/cache/persistence/CacheExpiryDateCalculator.java
+++ embers-services/src/main/java/adf/embers/cache/persistence/CacheExpiryDateCalculator.java
@@ -1,7 +1,7 @@
 package adf.embers.cache.persistence;
 
-import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.Date;
 
@@ -11,10 +11,16 @@ class CacheExpiryDateCalculator {
     private static final long MILLIS_IN_DAY = MILLIS_IN_HOUR * 24;
     private Date startingDate;
     private long millisToAdd;
+    private ZoneId zone;
 
-    public CacheExpiryDateCalculator(Date startingDate, long millisToAdd) {
+    public CacheExpiryDateCalculator(Date startingDate, long millisToAdd, ZoneId zone) {
         this.startingDate = startingDate;
         this.millisToAdd = millisToAdd;
+        this.zone = zone;
+    }
+
+    public CacheExpiryDateCalculator(Date startingDate, long millisToAdd) {
+        this(startingDate, millisToAdd, ZoneId.systemDefault());
     }
 
     public Date invoke() {
@@ -26,9 +32,9 @@ class CacheExpiryDateCalculator {
     }
 
     private Date calculatingFutureDateWithDateTime() {
-        LocalDateTime ldt = LocalDateTime.ofInstant(startingDate.toInstant(), ZoneId.systemDefault());
-        LocalDateTime plus = ldt.plus(millisToAdd, ChronoUnit.MILLIS);
-        return  Date.from(plus.atZone(ZoneId.systemDefault()).toInstant());
+        ZonedDateTime now = ZonedDateTime.ofInstant(startingDate.toInstant(), zone);
+        ZonedDateTime future = now.plus(millisToAdd, ChronoUnit.MILLIS);
+        return  Date.from(future.toInstant());
     }
 
     private boolean isCacheTimeBiggerThanIntegerMaxValueInDays() {
--- embers-services/src/test/java/adf/embers/cache/persistence/CacheExpiryDateCalculatorTest.java
+++ embers-services/src/test/java/adf/embers/cache/persistence/CacheExpiryDateCalculatorTest.java
@@ -3,6 +3,7 @@ package adf.embers.cache.persistence;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.time.Month;
 import java.time.ZoneId;
@@ -13,6 +14,8 @@ import static org.fest.assertions.api.Assertions.assertThat;
 
 public class CacheExpiryDateCalculatorTest {
 
+    private static final int ONE_HOUR_MS = (1000 * 60 * 60);
+    private final ZoneId london = ZoneId.of("Europe/London");
     private Date startingDate;
 
     @Before
@@ -23,26 +26,40 @@ public class CacheExpiryDateCalculatorTest {
     @Test
     //noticed odd behaviour when test randomly failed for a certain date,
     // turns out the api adds 1 hour when crosses daylight saving time
-    public void addingMillisWhenCrossesDST() throws Exception {
-        long millisToAdd = Integer.MAX_VALUE;
-        Date dateNearDst = createDate(2016, Month.OCTOBER, 9);
+    public void addingMillisWhenBstStarts() throws Exception {
+        long millisToAdd = ONE_HOUR_MS*48;
+        //day before BST starts
+        Date dateBeforeDst = createDate(2016, Month.MARCH, 26);
 
-        Date calculateFutureDate = new CacheExpiryDateCalculator(dateNearDst, millisToAdd).invoke();
+        Date calculated = new CacheExpiryDateCalculator(dateBeforeDst, millisToAdd, london).invoke();
 
-        assertThat(calculateFutureDate.getTime())
-                .describedAs("unexpected difference: "+(calculateFutureDate.getTime() - dateNearDst.getTime() - millisToAdd))
-                .isEqualTo(dateNearDst.getTime() + millisToAdd + (1000*60*60));
+        //48 hours later, +1 hour for DST.
+        assertThat(calculated).withDateFormat(new SimpleDateFormat("yyyy-MM-dd HH:mm")).isEqualTo("2016-3-28 02:02");
+    }
+
+    @Test
+    //noticed odd behaviour when test randomly failed for a certain date,
+    // turns out the api adds 1 hour when crosses daylight saving time
+    public void addingMillisWhenBstFallsBack() throws Exception {
+        long millisToAdd = ONE_HOUR_MS*48;
+        //the day before British Summer Time fallback
+        Date start = createDate(2016, Month.OCTOBER, 29);
+
+        Date calculated = new CacheExpiryDateCalculator(start, millisToAdd, london).invoke();
+
+        //48 hours later, -1 hour for DST fallback.
+        assertThat(calculated).withDateFormat(new SimpleDateFormat("yyyy-MM-dd HH:mm")).isEqualTo("2016-10-31 00:02");
     }
 
     @Test
     public void addingMillisOneMilliMoreThanMaxInteger() throws Exception {
-        long millisToAdd = Integer.MAX_VALUE+1l;
+        long millisToAdd = Integer.MAX_VALUE+1L;
         didItWork(startingDate, millisToAdd);
     }
 
     @Test
     public void addingOneYearOfMillis() throws Exception {
-        long millisToAdd = 1000*60*60*24*365;
+        long millisToAdd = 1000*60*60*24*365L;
         didItWork(startingDate, millisToAdd);
     }
 
@@ -55,7 +72,7 @@ public class CacheExpiryDateCalculatorTest {
 
     private Date createDate(final int year, final Month month, final int dayOfMonth) {
         LocalDateTime ldt = LocalDateTime.of(year, month, dayOfMonth, 1, 2);
-        return Date.from(ldt.atZone(ZoneId.systemDefault()).toInstant());
+        return Date.from(ldt.atZone(london).toInstant());
     }
 
     private void didItWork(Date startingDate, long millisToAdd) {
