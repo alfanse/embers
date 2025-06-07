dd13e8cbd46c7517bbe4c4be75c10cea00f595dd
alex <alex.fanshawe@gmail.com>
2025-06-07 08:56:01 +0100

    http4k adapter passing unit-test

--- embers-http4k/src/main/kotlin/adf/embers/examples/http4k/adapter/JakartaToHttp4kAdapter.kt
+++ embers-http4k/src/main/kotlin/adf/embers/examples/http4k/adapter/JakartaToHttp4kAdapter.kt
@@ -1,34 +1,34 @@
 package adf.embers.examples.http4k.adapter
 
+import org.http4k.core.Status
 import jakarta.ws.rs.core.Response as JaxRsResponse
 import org.http4k.core.Response as Http4kResponse
-import org.http4k.core.Status
 
 /**
  * Extension function to convert a Jakarta REST Response to an http4k Response
  */
 fun JaxRsResponse.toHttp4kResponse(): Http4kResponse {
-    val http4kResponse = Http4kResponse(Status(this.status, ""))
-    
+    var http4kResponse = Http4kResponse(Status(this.status, ""))
+
     // Copy headers
     this.headers.forEach { (key, values) ->
         values.forEach { value ->
-            http4kResponse.header(key, value.toString())
+            http4kResponse = http4kResponse.header(key, value.toString())
         }
     }
-    
+
     // Set body if present
     val entity = this.entity
-    return if (entity != null) {
+    if (entity != null) {
         val body = when (entity) {
             is String -> entity
             is ByteArray -> String(entity)
             else -> entity.toString()
         }
-        http4kResponse.body(body)
-    } else {
-        http4kResponse
+        http4kResponse = http4kResponse.body(body)
     }
+    
+    return http4kResponse
 }
 
 /**
